05899910e3362146373efe148fefb417821f4328
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-02-07 18:42:58 +0100

    This is going to be my first but not last commit!

--- P00/e4.py
+++ P00/e4.py
@@ -5,7 +5,7 @@ FRAT1 = seq_read_fasta("../sequences/FRAT1.txt")
 FXN_file = seq_read_fasta("../sequences/FXN.txt")
 bases = ["A", "C", "T", "G"]
 sequences = [U5_file, ADA_file, FXN_file, FRAT1]
-
+print("Gene U5")
 for a in sequences:
     for i in bases:
         print(str(i), seq_count(a, i))
