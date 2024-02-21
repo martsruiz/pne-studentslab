e4f26cac757fce7cb97d26fa36a55e834be702da
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-02-21 16:39:54 +0100

    This is going to be my first but not last commit!

--- P01/e6.py
+++ P01/e6.py
@@ -2,6 +2,6 @@ from Seq1 import Seq
 
 s1 = Seq()
 s2 = Seq("ACTGA")
-s3 = Seq("IATTC")
+s3 = Seq("IATTA")
 
 print("Sequence 1:", "(Length:", s1.len(), ")", s1 , "\n BASES:", s1.count(),"\nSequence 2:", "(Length:", s2.len(), ")", s2, "\n BASES:", s2.count(), "\nSequence 3:", "(Length:", s3.len(), ")", s3, "\n BASES:", s3.count())
