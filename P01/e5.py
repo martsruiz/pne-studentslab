81681576e0e0ee0866a3cf21a4ec99afdacfba1f
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-02-22 14:59:11 +0100

    This is going to be my first but not last commit!

--- P01/e5.py
+++ P01/e5.py
@@ -1,8 +1,8 @@
 from Seq1 import Seq
-
 s1 = Seq()
 s2 = Seq("ACTGA")
 s3 = Seq("IATTA")
 print(f"Sequence 0: (Length: {s1.len()}) {s1} \nSequence 1: (Length: {s2.len()}) {s2}\nSequence 2: (Length: {s3.len()}) {s3}")
+
 
 
