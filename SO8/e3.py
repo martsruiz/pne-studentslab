b146d86461b2dfa5cba5402574494efe820265bc
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-02-28 18:15:00 +0100

    This is going to be my first but not last commit!

---
+++ SO8/e3.py
@@ -0,0 +1,24 @@
+import socket
+
+# SERVER IP, PORT
+PORT = 1234
+IP = "212.128.255.90"#  depends on the computer the server is running tiene que ser el ID del ordenador al que queremos mandar
+ok = True
+while ok:
+  # -- Ask the user for the message
+    user_input = input("Please enter your message")
+
+  # -- Create the socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  # -- Establish the connection to the Server
+    s.connect((IP, PORT))
+
+  # -- Send the user message
+    s.send(str.encode(user_input))
+
+  # -- Close the socket
+    s.close()
+    ok = False
+
+
