1fefb53ada99cf2955b1a05cd05c78f18c432c2a
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-02-29 17:42:30 +0100

    This is going to be my first but not last commit!

--- P02/e4.py
+++ P02/e4.py
@@ -8,29 +8,29 @@ EXERCISE = 4
 genes = ["U5", "ADA", "FRAT1"]
 sequence = "../sequences/"
 
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
 # -- Parameters of the server to talk to
 IP = "212.128.255.90" # your IP address
 PORT = 1234
-def get_sequence():
-    for i in genes:
-        file = sequence + str(i) + ".txt"
-        s = Seq()
-        complete_file = s.read_fasta(file)
-    return complete_file
+def get_sequence(gene):
+    file_path = sequence + gene + ".txt"
+    s = Seq()
+    return s.read_fasta(file_path)
 
-sequences = get_sequence()
 c = Client(IP, PORT)
 print(c)
-# -- Send a message to the server
-for a in genes:
-    print(f"To server: Sending {a} Gene to the server...")
-    response = c.talk(f"Sending {a} Gene to the server...")
-    print(f"From Server: {response}")
-    for j in sequences:
-        print(f"To server:  {j} ")
-        response = c.talk(f"Sending {a} Gene to the server...")
-        print(f"From Server: {response}")
 
+
+# -- Send a message to the server
+for gene in genes:
+    sequences = get_sequence(gene)
+    print(f"To server: Sending {gene} Gene to the server...")
+    response = c.talk(f"Sending {gene} Gene to the server...")
+    print(f"From Server: {response}")
+    print(f"To server: {sequences} ")
+    response = c.talk(f" {sequences} ")
+    print(f"From Server: {response}")
 
 
 
