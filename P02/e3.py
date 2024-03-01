cc6efe04eb3895b3afae1758e2eb90a993941d6a
martaruizsaiz <m.ruizsa.2023@alumnos.urjc.es>
2024-03-01 10:09:46 +0100

    revisar el e5 la funcion get_fragments

--- P02/e3.py
+++ P02/e3.py
@@ -6,8 +6,9 @@ EXERCISE = 3
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
-IP = "12.128.255.91" # your IP address
+IP = "192.168.1.39" #"12.128.255.91" # your IP address
 PORT = 1234
+
 
 # -- Create a client object
 c = Client(IP, PORT)
---
+++ P02/e5.py
@@ -0,0 +1,54 @@
+from Client0 import Client
+from Seq1 import Seq
+from pprint import pprint
+
+
+PRACTICE = 2
+EXERCISE = 5
+genes = "FRAT1"
+sequence = "../sequences/"
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "192.168.1.39"  #"212.128.255.90" # your IP address
+PORT = 1234
+def get_sequence(gene):
+    file_path = sequence + gene + ".txt"
+    s = Seq()
+    return s.read_fasta(file_path)
+
+c = Client(IP, PORT)
+print(c)
+
+
+# -- Send a message to the server
+sequences = get_sequence(genes) #sequencia completa del gen
+
+def get_fragments(sequences):
+    count = 0
+    fragments = ""
+    rounds = 0
+    for i in sequences:
+        count += 1
+        fragments += i
+        if count == 10:
+            print(f" Fragment {rounds + 1}: {fragments}")
+            rounds += 1
+            count = 0
+            fragments = ""
+        if rounds == 5:
+            break
+
+
+print(f"Gene {genes}: {sequences}")
+response = c.talk(f"Sending {genes} Gene to the server, in fragments of 10 bases...")
+
+fragments = get_fragments(sequences)
+
+print(fragments)
+
+response = c.talk(fragments)
+
+
+
