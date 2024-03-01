d4adec580ea32e9853fd069645f11a878bcedbbc
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-03-01 16:00:29 +0100

    This is going to be my first but not last commit!

--- P02/e5.py
+++ P02/e5.py
@@ -11,7 +11,7 @@ sequence = "../sequences/"
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
-IP = "192.168.1.39"  #"212.128.255.90" # your IP address
+IP = "212.128.255.90"  #"212.128.255.90" # your IP address
 PORT = 1234
 def get_sequence(gene):
     file_path = sequence + gene + ".txt"
@@ -26,30 +26,28 @@ print(c)
 sequences = get_sequence(genes) #sequencia completa del gen
 
 def get_fragments(sequences):
-    count = 0
     fragments = ""
     rounds = 0
+    list = []
     for i in sequences:
-        count += 1
         fragments += i
-        if count == 10:
-            print(f" Fragment {rounds + 1}: {fragments}")
+        if len(fragments) == 10:
+            list.append(fragments)
             rounds += 1
-            count = 0
             fragments = ""
         if rounds == 5:
             break
-n = 0
+    return list
+
+
+
+
 
 
 print(f"Gene {genes}: {sequences}")
 response = c.talk(f"Sending {genes} Gene to the server, in fragments of 10 bases...")
 
 fragments = get_fragments(sequences)
-
-print(fragments)
-
-response = c.talk(fragments)
-
-
-
+for i, fragment in enumerate(fragments, start=1):
+    print(f"Fragment {i}: {fragment}")
+    response = c.talk(f"Fragment {i}: {fragment}")
