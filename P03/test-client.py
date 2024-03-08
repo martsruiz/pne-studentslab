222567ed8fa383dfa2216a7225e4992271c7072d
martarui <m.ruizsa.2023@alumnos.urjc.es>
2024-03-08 15:14:59 +0100

    This is going to be my first but not last commit!

--- P03/test-client.py
+++ P03/test-client.py
@@ -1,5 +1,15 @@
-from Client0 import Clients
+from Client0 import Client
 IP = "127.0.0.1"
 PORT = 12345
 c = Client(IP, PORT)
 print(c)
+print("Testing Ping...")
+print(c.talk("PING"))
+print("Testing GET...")
+print(c.talk("GET"))
+print("Testing Info...")
+print(c.talk("INFO"))
+print("Testing COMP...")
+print(c.talk("COMP"))
+print("Testing REV...")
+print(c.talk("REV"))
