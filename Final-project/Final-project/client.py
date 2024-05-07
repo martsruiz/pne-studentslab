831df01c359550866068794979b8f61b9c720715
martaruizsaiz <m.ruizsa.2023@alumnos.urjc.es>
2024-05-07 16:40:15 +0200

    revisar el e5 la funcion get_fragments

---
+++ Final-project/Final-project/client.py
@@ -0,0 +1,33 @@
+
+import http.client
+import json
+import termcolor
+
+PORT = 8080
+SERVER = 'localhost'
+
+print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", "/listusers")
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Create a variable with the data,
+# -- form the JSON received
+people = json.loads(data1)
--- Final-project/Final-project/server.py
+++ Final-project/Final-project/server.py
@@ -70,6 +70,8 @@ def info_response(msg):
     return info_list
 
 
+
+
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -87,6 +89,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         print(path)
         print(arguments)
         contents = ""
+        json_arguments = arguments["json"][0]
         if path == "/":
             contents = Path("index.html").read_text()
 
@@ -186,24 +189,51 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             chromosome = arguments["chromo"][0]
             start = arguments["start"][0]
             end = arguments ["end"][0]
-            ENDPOINT = f"/overlap/region/human/{chromosome}:{start}-{end}/gene"
-            
-            gene_info = connect_server(ENDPOINT)
-            contents = read_html_file("gene_list.html")
+            endpoint = f"/phenotype/region/homo_sapiens/{chromosome}:{start}-{end}"
+            gene_info = connect_server(endpoint)
+            list_of_genes = []
+            for gene in gene_info:
+                gene_name = gene["id"]
+                list_of_genes.append(gene_name)
+
+            dic_info = {"user_chromo": chromosome, "start_position": start, "end_position": end, "list_of_genes": list_of_genes}
+
+            contents = read_html_file("gene_list.html").render(
+                context= dic_info)
+            with open("gene_list", "w") as f:
+                json.dumps(dic_info,f)
+                f.close()
+            contents = Path("gene_list").read_text()
 
 
-        # Generating the response message
-        self.send_response(200)  # -- Status line: OK!
 
-        # Define the content-type header:
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(str.encode(contents)))
+        if json_arguments == 0:
 
-        # The header is finished
-        self.end_headers()
+            # Generating the response message
+            self.send_response(200)  # -- Status line: OK!
 
-        # Send the response message
-        self.wfile.write(str.encode(contents))
+            # Define the content-type header:
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+
+            # The header is finished
+            self.end_headers()
+
+            # Send the response message
+            self.wfile.write(str.encode(contents))
+
+        elif json_arguments == 1:
+            self.send_response(200)  # -- Status line: OK!
+
+            # Define the content-type header:
+            self.send_header('Content-Type', 'application/json')
+            self.send_header('Content-Length', len(str.encode(contents)))
+
+            # The header is finished
+            self.end_headers()
+
+            # Send the response message
+            self.wfile.write(str.encode(contents))
 
 
 
